4831a0fa5fdf303858ad9b9ca9b180b5d392610a
Matheus Rian <souzamatheusrian@gmail.com>
2020-11-26 01:01:08 -0200

    Finalizando Featured

--- src/components/FeaturedMovie/index.jsx
+++ src/components/FeaturedMovie/index.jsx
@@ -1,18 +1,56 @@
-import React from 'react'
-import {Featured, FeaturedTransparencyVertical} from './styles'
+import React from "react";
+import {
+  Featured,
+  FeaturedTransparencyVertical,
+  FeaturedTransparencyHorizontal,
+  FeaturedName,
+  FeaturedInfo,
+  FeaturedPoints,
+  FeaturedYear,
+  FeaturedSeasons,
+  FeaturedDescription,
+  FeaturedButtons,
+  FeaturedGenres,
+} from "./styles";
 
-function FeaturedMovie ({item}) {
+function FeaturedMovie({ item }) {
+
+  let firstDate = new Date(item.first_air_date);
+  let genres = []
+  for(let i in item.genres) {
+    genres.push(item.genres[i].name)
+  }
   return (
-    <Featured style={{
-      backgroundSize: 'cover',
-      backgroundPosition: 'center',
-      backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
-    }}>
+    <Featured
+      style={{
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`,
+      }}
+    >
       <FeaturedTransparencyVertical>
-
+        <FeaturedTransparencyHorizontal>
+          <FeaturedName>{item.original_name}</FeaturedName>
+          <FeaturedInfo>
+            <FeaturedPoints>{item.vote_average} pontos</FeaturedPoints>
+            <FeaturedYear>{firstDate.getFullYear()}</FeaturedYear>
+            <FeaturedSeasons>
+              {item.number_of_seasons} temporada
+              {item.number_of_seasons > 1 ? "s" : ""}
+            </FeaturedSeasons>
+          </FeaturedInfo>
+          <FeaturedDescription>{item.overview}</FeaturedDescription>
+          <FeaturedButtons>
+            <a href={`/watch/${item.id}`} className="watch">▶ Assistir</a>
+            <a href={`/list/add/${item.id}`} className="list">+ Minha Lista</a>
+          </FeaturedButtons>
+          <FeaturedGenres>
+            <strong>Gêneros: </strong>{genres.join(', ')}
+          </FeaturedGenres>
+        </FeaturedTransparencyHorizontal>
       </FeaturedTransparencyVertical>
     </Featured>
-  )
+  );
 }
 
 export default FeaturedMovie;
--- src/components/FeaturedMovie/styles.js
+++ src/components/FeaturedMovie/styles.js
@@ -5,5 +5,94 @@ export const Featured = styled.section`
 `;
 
 export const FeaturedTransparencyVertical = styled.div`
-
+  width: inherit;
+  height: inherit;  
+  /* inherit: Vai herdar o tamanho do anterior(Featured) */
+  background: linear-gradient(to top, #111 10%, transparent 90%);
 `;
+
+export const FeaturedTransparencyHorizontal = styled.div`
+  width: inherit;
+  height: inherit;  
+  background: linear-gradient(to right, #111 30%, transparent 70%);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding-left: 30px;
+`;
+
+export const FeaturedName = styled.div`
+  font-size: 60px;
+  font-weight: bold;
+`;
+
+export const FeaturedInfo = styled.div`
+  font-size: 18px;
+  font-weight: bold;
+  margin-top: 15px;
+`;
+
+export const FeaturedPoints = styled.div`
+  display: inline-block; //Coloca um do lado da outra e o block nos permite colocar width e heigth
+  color: #46d369;
+  margin-right: 15px;
+`;
+
+export const FeaturedYear = styled.div`
+  display: inline-block;
+  margin-right: 15px;
+`;
+
+export const FeaturedSeasons = styled.div`
+  display: inline-block;
+`;
+
+export const FeaturedDescription = styled.div`
+  margin-top: 15px;
+  font-size: 20px;
+  color: #999;
+  max-width: 40%;
+`;
+
+export const FeaturedButtons = styled.div`
+    margin-top: 15px;
+
+    .watch {
+      display:inline-block;
+      font-size: 20px;
+      font-weight: bold;
+      padding: 12px 25px;
+      border-radius: 5px;
+      text-decoration: none;
+      margin-right: 10px;
+      background-color: #FFF;
+      color: #000;
+      opacity: 1;
+      transition: all ease 0.2s;
+    }
+
+    .list {
+      display:inline-block;
+      font-size: 20px;
+      font-weight: bold;
+      padding: 12px 25px;
+      border-radius: 5px;
+      text-decoration: none;
+      margin-right: 10px;
+      background-color: #333;
+      color: #FFF;
+      opacity: 1;
+      transition: all ease 0.2s;
+    }
+
+    .watch:hover, .list:hover {
+      opacity: 0.7;
+    }
+`;
+
+export const FeaturedGenres = styled.div`
+  margin-top: 15px;
+  font-size: 18px;
+  color: #999;
+`;
+
